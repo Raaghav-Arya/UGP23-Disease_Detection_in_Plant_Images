2aefbd37cf0d5084559a2400b36d7f5a25975a55
Raaghav-Arya <raaghava20@iitk.ac.in>
2023-11-07 15:36:03 +0530

    Upload buttons better. Now images displayed after upload. Need better layout. Need to reroute post pressing upload button
    Signed-off-by: Raaghav-Arya <raaghava20@iitk.ac.in>

--- front/src/App.js
+++ front/src/App.js
@@ -6,13 +6,13 @@ function App() {
     <>
     <header className='App-header'>
       <nav>
-        <img class="brand"  src={require("./CropShield_logo.png")} alt="CropShield.AI Logo"></img>
-        <div>
-          
-        </div>
+        <img className="brand"  src={require("./CropShield_logo.png")} alt="CropShield.AI Logo"></img>
       </nav>
     </header>
-
+    <div className="home-text" >
+      <h1>Upload images to get started!</h1>
+      <p>Our Machine Learning Platform will analyze your images and provide you with a detailed report of your crop's health.</p>
+    </div>
     <div className="App">
       <ImageUpload />
     </div>
--- front/src/ImageUpload.jsx
+++ front/src/ImageUpload.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import JSZip from 'jszip'
-import { Button } from '@mui/material';
+import { Button } from 'antd';
+import UploadButton from './UploadButton';
+import { UploadOutlined } from '@ant-design/icons';
 
 axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
 const ImageUpload = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [images, setImages] = useState(null);
+  const fileInputRef = useRef(null);
 
   const onDrop = (event) => {
     setSelectedFiles([...event.target.files]);
   };
 
   const uploadImages = async () => {
+    console.log(selectedFiles)
     const formData = new FormData();
 
     selectedFiles.forEach((file) => {
@@ -28,7 +32,7 @@ const ImageUpload = () => {
 
       const jszip = new JSZip();
       const zip = await jszip.loadAsync(response.data)
-      const files = await Promise.all(Object.keys(zip.files).map(async filename => 
+      const files = await Promise.all(Object.keys(zip.files).map(async filename =>
         await zip.files[filename].async('base64')
       ))
       setImages(files)
@@ -37,13 +41,47 @@ const ImageUpload = () => {
       console.error('Error uploading images:', error);
     }
   };
+  const handleImageRemove = index => {
+    let imagesArray = [...selectedFiles];
+    imagesArray.splice(index, 1);
+    setSelectedFiles(imagesArray);
+  };
 
   return (
     <div>
-      <input type='file' multiple onChange={onDrop} accept='image/*' />
-      <Button >Upload Images</Button>
-      {images && images.map(image => <img key={image} src={'data:image/jpeg;base64,'+image} />)}
+      <Button onClick={() => fileInputRef.current.click()} icon={<UploadOutlined />}>Click to Upload</Button>
+      <input ref={fileInputRef} style={{ display: 'none' }} type='file' multiple onChange={onDrop} accept='image/*' />
+      {selectedFiles.length > 0 && <Button onClick={uploadImages}>Upload Images</Button>}
+      {console.log(selectedFiles)}
+
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {selectedFiles.map((file, index) => (
+        <div key={index} style={{ position: 'relative', margin: '10px' }}
+          onMouseOver={(e) => e.currentTarget.children[1].style.display = 'block'}
+          onMouseOut={(e) => e.currentTarget.children[1].style.display = 'none'}
+        >
+          <img src={URL.createObjectURL(file)} alt="" style={{ width: '100px', height: '100px' }} />
+          <button onClick={() => handleImageRemove(index)} style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            backgroundColor: 'red',
+            color: 'white',
+            border: 'none',
+            cursor: 'pointer',
+            borderRadius: '50%',
+            width: '20px',
+            height: '20px',
+            display: 'none'
+          }}>X</button>
+        </div>
+      ))}
     </div>
+
+      {images && images.map(image => <img key={image} src={'data:image/jpeg;base64,' + image} />)}
+      {/* <UploadButton setFileList={setSelectedFiles} fileList={selectedFiles} /> */}
+    </div>
+
   );
 };
 
