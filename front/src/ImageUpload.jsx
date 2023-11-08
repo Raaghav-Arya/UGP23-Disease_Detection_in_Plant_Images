4b9452a189fb83806bbc89a75bf2da099dd6a33e
Raaghav-Arya <raaghava20@iitk.ac.in>
2023-11-08 08:20:51 +0530

    Uploaded images diplayed properly. UI could be better.

--- front/src/ImageUpload.jsx
+++ front/src/ImageUpload.jsx
@@ -54,28 +54,42 @@ const ImageUpload = () => {
       {selectedFiles.length > 0 && <Button onClick={uploadImages}>Upload Images</Button>}
       {console.log(selectedFiles)}
 
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {selectedFiles.map((file, index) => (
-        <div key={index} style={{ position: 'relative', margin: '10px' }}
-          onMouseOver={(e) => e.currentTarget.children[1].style.display = 'block'}
-          onMouseOut={(e) => e.currentTarget.children[1].style.display = 'none'}
-        >
-          <img src={URL.createObjectURL(file)} alt="" style={{ width: '100px', height: '100px' }} />
-          <button onClick={() => handleImageRemove(index)} style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            backgroundColor: 'red',
-            color: 'white',
-            border: 'none',
-            cursor: 'pointer',
-            borderRadius: '50%',
-            width: '20px',
-            height: '20px',
-            display: 'none'
-          }}>X</button>
-        </div>
-      ))}
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      paddingTop: '20px'
+    }}>
+      <div style={{ 
+        display: 'grid', 
+        gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))', 
+        gap: '10px', 
+        width: '100%', 
+        maxWidth: '600px'  // Adjust this value to your needs
+      }}>
+        {selectedFiles.map((file, index) => (
+          <div key={index} style={{ position: 'relative' }}
+            onMouseOver={(e) => e.currentTarget.children[1].style.display = 'block'}
+            onMouseOut={(e) => e.currentTarget.children[1].style.display = 'none'}
+          >
+            <img src={URL.createObjectURL(file)} alt="" style={{ width: '100%', height: 'auto'}} />
+            <button onClick={() => handleImageRemove(index)} style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              backgroundColor: 'red',
+              color: 'white',
+              border: 'none',
+              cursor: 'pointer',
+              borderRadius: '50%',
+              width: '20px',
+              height: '20px',
+              display: 'none'
+            }}>X</button>
+          </div>
+        ))}
+      </div>
     </div>
 
       {images && images.map(image => <img key={image} src={'data:image/jpeg;base64,' + image} />)}
