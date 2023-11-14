d022faefb2271fccff9ddf7f3f4c2e5dcba66997
Raaghav-Arya <raaghava20@iitk.ac.in>
2023-11-14 23:40:41 +0530

    Images diplayed nicely like a carousel in result

--- front/src/pages/Result.jsx
+++ front/src/pages/Result.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import { useMyContext } from "../MyContext";
 
 const Result = () => {
   const {result, setResult} = useMyContext()
-  return <>
-    <h1 style={{textAlign: "center"}}>Your plants' Health Report:</h1>
-    {result && result.map(image => <img key={image} src={'data:image/jpeg;base64,' + image} />)}
-  </>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((currentIndex + 1) % result.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((currentIndex - 1 + result.length) % result.length);
+  };
+
+  return (
+    <>
+      <h1 style={{textAlign: "center"}}>Your plants' Health Report:</h1>
+      {result && result.length > 0 && (
+        <div style={{textAlign: "center"}}>
+          <img src={'data:image/jpeg;base64,' + result[currentIndex]} style={{width: 'auto', height: '300px'}} />
+          <div>
+            <button onClick={handlePrev} style={{marginRight: '10px'}}>←</button>
+            <button onClick={handleNext}>→</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Result;
